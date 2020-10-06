e819ea63e50d9147ecb005daab80d75c4e0fa798
Crius Gamora <cgamora@mi-n5.msk.21-school.ru>
2020-10-06 18:02:21 +0300

    g

---
+++ first.php
@@ -0,0 +1,48 @@
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Начало начал</title>
+ </head>
+ <body>
+  <h1>Уши</h1>
+  <!-- Комментарий -->
+  <p align="left"><font color="orange" face="fantasy">Кро</font></p>
+  <hr 	size = 10
+		width = 1800
+		color = green
+		align = justify>
+  <p align="center"><font color="green" face="serif">Ле</font></p>
+  <hr 	size = 10
+		width = 1800
+		color = yellow
+		align = justify>
+  <p align="right"><font color="black" face="cursive">Чка</font></p>
+  <hr 	size = 10
+		width = 1800
+		color = orange
+		align = justify>
+  <p align="justify"><blink>Четвертый абзац.</blink></p>
+  <?php
+  date_default_timezone_set('UTC');
+  echo date("l");
+  echo date('l jS \of F Y h:i:s A'); ?>
+  <menu>
+  <li><b>K</b><i>e</i><u>k</u></li>
+  <li>Lul</li>
+  <form>
+  <table>
+  <tr>
+  <td><button formaction="https://ru.wikipedia.org/wiki/%D0%9A%D1%80%D0%BE%D0%BB%D0%B8%D0%BA%D0%B8" formtarget="new"><img src="https://www.belanta.vet/vet-blog/wp-content/uploads/2016/02/soderzhanie-dekorativnogo-krolika-02.jpg" height="255" width="340"></button><br></td>
+  <td><button formaction="https://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%81%D0%B8%D1%86%D0%B0" formtarget="neww"><img src="https://ewscripps.brightspotcdn.com/dims4/default/98c088f/2147483647/strip/true/crop/2450x1378+0+170/resize/1280x720!/quality/90/?url=https%3A%2F%2Fmediaassets.wtkr.com%2Ftribune-network%2Ftribwtkr-files-wordpress%2F2018%2F07%2Fgettyimages-516318760.jpg" height="255" width="340"></button><br></td>
+  <td><button formaction="https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D1%88%D0%BA%D0%B0" formtarget="newwe"><img src="https://cdn24.img.ria.ru/images/155231/05/1552310590_0:405:2048:1557_600x0_80_0_0_880832a40040dfcb928722ae168cc719.jpg" height="255" width="340"></button><br></td>
+  </tr>
+  <tr>
+  <td><p align="center">#1 IS Rabbit</p></td>
+  <td><p align="center">#2 IS Fox</td>
+  <td>#3 IS Cat</p></td>
+  </tr>
+  </table>
+  </form>
+  </menu>
+ </body>
+</html>
